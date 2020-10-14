e58aee32ce1d6dd73d84d7464e2f7c94f25415f1
Kiryl Yermalovich <ts.underawater@gmail.com>
2020-10-14 18:36:24 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = [];
+    
+  const brackets = Object.fromEntries(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+      let current = str[i];
+      if (arr[arr.length-1] == current) arr.pop();
+      else if (brackets[current]) arr.push(brackets[current]);
+      else return false;
+  }
+  return (stack.length) ? false : true
 }
